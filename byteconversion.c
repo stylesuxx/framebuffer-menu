487cd63149b71c2e2acc16ea6ab158877bd7c2ac
stylesuxx <stylesuxx@gmail.com>
2014-04-27 03:14:25 +0200

    Pass length of array as parameter.

--- byteconversion.c
+++ byteconversion.c
@@ -1,11 +1,13 @@
+#include <stdio.h>
+
 #include "byteconversion.h"
 
-unsigned int littleEndianToInt(unsigned char bytes[])
+unsigned int littleEndianToInt(unsigned char bytes[], int length)
 {
   unsigned int result = 0;
 
   int i;
-  for(i = 0; i < sizeof(bytes); i++)
+  for(i = 0; i < length; i++)
     result |= bytes[i]<<(8*i);
 
   return result;
--- byteconversion.h
+++ byteconversion.h
@@ -1,6 +1,6 @@
 #ifndef BYTECONVERSION_H
 #define BYTECONVERSION_H
 
-unsigned int littleEndianToInt(unsigned char bytes[]);
+unsigned int littleEndianToInt(unsigned char bytes[], int length);
 
 #endif
